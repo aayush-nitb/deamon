3cb1ccd90bc79bd4e36982fd18dbd029f8a55432
Aayush Agrawal <aaagrawa@adobe.com>
2017-03-16 03:24:02 +0530

    use component level styles using less

--- src/app/components/subscription/App_Subscription.ts
+++ src/app/components/subscription/App_Subscription.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector    : 'app-subscription',
+    styleUrls   : ['./less/App_Subscription.less'],
     template    : `
         <h2>Subscribe</h2>
         <input app-commons-autogrow="300px" width="100px"/>
