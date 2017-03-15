a33c6fa0b4e6ca2649790e97b93c34971169a098
Aayush Agrawal <aaagrawa@adobe.com>
2017-03-16 05:21:57 +0530

    subscription icon

--- src/app/components/subscription/App_Subscription.ts
+++ src/app/components/subscription/App_Subscription.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core'
     selector    : 'app-subscription',
     styleUrls   : ['./less/App_Subscription.less'],
     template    : `
-        <h2>Subscribe</h2>
-        <input app-commons-autogrow="300px" width="100px"/>
+        <img src="assets/subscription/mail.png" class="icon"/>
+        <div class="content">
+            <h2>Subscribe</h2>
+            <input app-commons-autogrow="300px" width="200px"/>
+        </div>
     `
 })
 export class App_Subscription {
